ea06a460c0a98be38d2e254edc0242835d5191c7
Anna Hill <alouiseh14@gmail.com>
2018-04-04 15:50:22 -0600

    playing around

--- P3/other_2/sketch.js
+++ P3/other_2/sketch.js
@@ -3,38 +3,24 @@ var amp;
 
 var scale = 1.0;
 
-
-var cnv, mic, fft, peakDetect;
-var ellipseWidth = 10;
+var ellipseWidth = 30;
 
 function setup() {
   createCanvas (windowWidth, windowHeight);
-  background(0);
   noStroke();
   fill(255);
 
   mic = new p5.AudioIn();
   mic.start();
 
-  // p5.PeakDetect requires a p5.FFT
-  fft = new p5.FFT();
-  peakDetect = new p5.PeakDetect();
-  fft.setInput(mic);
+  amp = new p5.Amplitude();
+  amp.setInput(mic);
+  amp.smooth(0.9);
+  getLevel([smoothing]);
 
 }
 
 function draw() {
   background(0);
-
-  // peakDetect accepts an fft post-analysis
-  fft.analyze();
-  peakDetect.update(fft);
-
-  if ( peakDetect.isDetected ) {
-    ellipseWidth = 50;
-  } else {
-    ellipseWidth *= 0.95;
-  }
-
-  ellipse(width/2, height/2, ellipseWidth, ellipseWidth);
+  ellipse(width/2, height/2, amp.getLevel, ellipseWidth);
 }
